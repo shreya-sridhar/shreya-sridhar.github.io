902f3a07cdd9d8651aeb92008c2adb7dae1fd214
Shreya Sridhar <shreyamohak@outlook.com>
2021-05-04 15:15:06 -0700

    Update Work.js

--- src/data/Work.js
+++ src/data/Work.js
@@ -1,7 +1,7 @@
-import flyrobe from "../assets/images/flyrobe-logo.png";
-import vodafone from "../assets/images/vodafone-logo.png";
-import simeio from "../assets/images/simeio-logo.png";
-import ola from "../assets/images/ola-logo.png";
+#import flyrobe from "../assets/images/flyrobe-logo.png";
+#import vodafone from "../assets/images/vodafone-logo.png";
+#import simeio from "../assets/images/simeio-logo.png";
+#import ola from "../assets/images/ola-logo.png";
 
 export default [
 	{
@@ -9,7 +9,7 @@ export default [
 		name: "Ola (ANI Technologies Pvt. Ltd.)",
 		designation: "Analyst / Programmer",
 		dated: "Mar 2018 - Dec 2019 ",
-		logo: ola,
+// 		#logo: ola,
 		companyLink: "https://en.wikipedia.org/wiki/Ola_Cabs",
 		description:
 			"<p><i>Ola is India’s largest mobility platform and one of the world’s largest ride-hailing companies, serving 250+ cities across India, Australia, New Zealand, and the UK. The Ola app offers mobility solutions by connecting customers to drivers and a wide range of vehicles across bikes, auto-rickshaws, metered taxis, and cabs, enabling convenience and transparency for hundreds of millions of consumers and over 1.5 million driver-partners.</i></p>"
@@ -19,7 +19,7 @@ export default [
 		name: "Simeio Solutions",
 		designation: "Analytics Lead",
 		dated: "July 2016 - Feb 2018",
-		logo: simeio,
+// 		# logo: simeio,
 		companyLink: "https://simeio.com/",
 		description:
 			"<p><i>An innovative and industry leader, Simeio offers professional services, Identity and Access Management (IAM) managed services and Identity as a Service (IDaaS). </i></p>"
@@ -29,7 +29,7 @@ export default [
 		name: "Flyrobe",
 		designation: "Web Developer",
 		dated: "April 2016 - July 2016",
-		logo: flyrobe,
+// 		# logo: flyrobe,
 		companyLink: "https://flyrobe.com/",
 		description:
 			"<p><i>Flyrobe is India's largest fashion rental platform offering occasion wear on rent for men and women. Its present in 10 major Indian cities and also has offline stores in Mumbai and Delhi.</i></p>"
@@ -39,7 +39,7 @@ export default [
 		name: "RoundGlass ZoojooBe",
 		designation: "Frontend Developer Intern",
 		dated: "Jan 2016 - May 2016",
-		logo: vodafone,
+// 		# logo: vodafone,
 		companyLink: "https://www.zoojoo.be",
 		description:
 			"<p>Zoojoo.be is a game based social wellness platform that unites the workforce to form healthier habits. The psychology behind Zoojoo.be is to enable peer-to-peer motivation allowing people to challenge, cheer and reward their colleagues and friends within their organisation. <p>Contribution : <p> <ul><li>Performance improvements to the platform. This helped reduce the page load time from 12 seconds to just 4 seconds.</li><li>Worked on multiple client specific feature requirements on the platform, for companies like HP, HPI, Mphasis, Sapient, Mindtree and more.</li><li>Developed and maintained the frontend of the sales website for Aware, a guided mindfulness meditation mobile app.</li></ul>"
