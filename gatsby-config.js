d30ce3dc8083df8ab113a1f41ba30821f152e89f
shreya-sridhar <shreyamohak@outlook.com>
2021-05-04 13:34:41 -0700

    sth

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Shreya Sridhar',
-    siteUrl: 'https://shreya-sridhar.github.io',
+    // siteUrl: 'https://shreya-sridhar.github.io',
   },
 
   plugins: [
