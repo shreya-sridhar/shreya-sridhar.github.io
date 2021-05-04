96e0a93471a6896b2d6f9e4d6f3de02a0c360914
Shreya Sridhar <shreyamohak@outlook.com>
2021-05-04 16:46:28 -0700

    Revert "Develop"

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"UA-130172537-1","head":false},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
